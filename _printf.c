d16d2b86ca7e7b4d4381ed1393ff152243b562b6
rania-elh <rania1elhefnawy@gmail.com>
2024-11-26 15:37:31 +0100

    Rania

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_printf - produces output according to a format
+ *@format: pointer
+ *Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	
