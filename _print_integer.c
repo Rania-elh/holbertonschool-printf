39020d9d19ee2af5853015d2115554ea8d9de4dd
root <root@LAPTOP-3F3BGL77.>
2024-11-29 18:07:29 +0100

    1

--- _print_integer.c
+++ _print_integer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _print_int - print integers
@@ -9,14 +10,14 @@
 int print_int(va_list args)
 {
 int count = 0;
-int n = va_arg(args , int);
+int n = va_arg(args, int);
 if (n < 0)
 {
 	_putchar('-');
 	n = -n;
 	count++;
 }
-if (n / 10 != 0 )
+if (n / 10 != 0)
 count += print_int(args);
 _putchar((n % 10) + '0');
 count++;
